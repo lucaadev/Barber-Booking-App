42e952c4afff18bb0864d2538e50b6fa47c5224e
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-10 16:13:19 -0300

    FEAT: UpdateServico & deleteArquivo controllers

--- ws/src/controller/servicoController.js
+++ ws/src/controller/servicoController.js
@@ -1,4 +1,8 @@
-const { handleFileUpload } = require("../services/servicoService");
+const {
+  handleFileUpload,
+  updateServico,
+  deleteArquivo,
+} = require("../services/servicoService");
 
 const newServico = async (req, res, next) => {
   try {
@@ -9,4 +13,22 @@ const newServico = async (req, res, next) => {
   }
 };
 
-module.exports = { newServico };
+const updatedServico = async (req, res, next) => {
+  try {
+    const servico = await updateServico(req);
+    return res.status(200).json(servico);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const removeServico = async (req, res, next) => {
+  try {
+    const message = await deleteArquivo(req);
+    return res.status(200).json(message);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { newServico, updatedServico, removeServico };
