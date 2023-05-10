ef057e20b804b7162cbff2e3acd60d552be053de
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-10 16:12:12 -0300

    FEAT: add UpdateServico & deleteArquivo routes

--- ws/src/routes/servico.route.js
+++ ws/src/routes/servico.route.js
@@ -4,6 +4,8 @@ const servicoController = require('../controller/servicoController');
 const router = express.Router();
 
 router.post('/', servicoController.newServico);
+router.put('/:id', servicoController.updatedServico);
+router.delete('/delete-arquivo', servicoController.removeServico);
 
 
 module.exports = router;
