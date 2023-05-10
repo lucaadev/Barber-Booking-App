cc859ad5943fd2853dd5619b2da898f8d79493b1
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-09 23:45:13 -0300

    FEAT: create a upload to aws function

---
+++ ws/src/utils/aws/aws.js
@@ -0,0 +1,71 @@
+const { Upload } = require("@aws-sdk/lib-storage");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3 } = require("@aws-sdk/client-s3");
+const fs = require('fs');
+require('dotenv').config();
+
+module.exports = {
+  IAM_USER_KEY: process.env.USER_KEY,
+  IAM_USER_SECRET: process.env.USER_SECRET,
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  AWS_REGION: process.env.AWS_REGION,
+  uploadToS3: async function(file, filename) {
+      const IAM_USER_KEY = this.IAM_USER_KEY;
+      const IAM_USER_SECRET = this.IAM_USER_SECRET;
+      const BUCKET_NAME = this.BUCKET_NAME;
+      const AWS_REGION = this.AWS_REGION;
+      
+      let s3Client = new S3Client({
+        region: AWS_REGION,
+        credentials: {
+          accessKeyId: IAM_USER_KEY,
+          secretAccessKey: IAM_USER_SECRET,
+        }
+      });
+
+      const fileContent = fs.readFileSync(file.path);
+      const fileBuffer = Buffer.from(fileContent);
+
+      var params = {
+        Bucket: BUCKET_NAME,
+        Key: filename,
+        Body: fileBuffer,
+      };
+
+      try {
+        const command = new PutObjectCommand(params);
+        const data = await s3Client.send(command);
+        console.log(data);
+        return { error: false, message: data };
+      } catch (err) {
+        console.log(err);
+        return { error: true, message: err.message };
+      }
+  },
+  deleteFileS3: function(key) {
+    let IAM_USER_KEY = this.IAM_USER_KEY;
+    let IAM_USER_SECRET = this.IAM_USER_SECRET;
+    let BUCKET_NAME = this.BUCKET_NAME;
+
+    let s3bucket = new S3({
+      accessKeyId: IAM_USER_KEY,
+      secretAccessKey: IAM_USER_SECRET,
+      Bucket: BUCKET_NAME,
+    });
+
+      s3bucket.deleteObject(
+        {
+        Bucket: BUCKET_NAME,
+        Key: key, 
+        },
+        function (err, data) {
+          if (err) {
+            console.log(err);
+            return resolve({ error: true, message: err });
+          }
+          console.log(data);
+          return resolve({ error: false, message: data });
+        }
+      );
+  },
+};
