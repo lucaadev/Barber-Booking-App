c0ab81e4b71bc77e3336c039f3871934567f63aa
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-15 21:43:32 -0300

    FIX: organizing and grouping requests

--- ws/src/routes/salao.js
+++ ws/src/routes/salao.js
@@ -3,10 +3,13 @@ const salaoController = require('../controller/salao');
 const salaoValidation = require('../middlewares/salaoValidation');
 const router = express.Router();
 
-router.post('/', salaoValidation, salaoController.newSalao);
 router.get('/servicos/:salaoId', salaoController.servicosSalao);
 router.get('/:salaoId', salaoController.getInfoSalaoById);
+
+router.post('/', salaoValidation, salaoController.newSalao);
+
 router.put('/colaborador/:id', salaoController.upColaborador);
+
 router.delete('/colaborador/status/:id', salaoController.changeStatusColaborador);
 
 
