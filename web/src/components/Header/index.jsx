2cf7fe90caa8f61fc07b9c202fa0b5c7a7d7d460
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-23 15:17:25 -0300

    FIX: change logo image

--- web/src/components/Header/index.jsx
+++ web/src/components/Header/index.jsx
@@ -1,3 +1,5 @@
+import logoPrincipal from '../../assets/logo-principal.png';
+
 const Header = () => {
   return (
     <header className="container-fluid d-flex justify-content-end" >
@@ -6,7 +8,7 @@ const Header = () => {
         <span className="d-block m-0 p-0 text-white">Barbearia Lisboa</span>
         <small className="m-0 p-0">Plano Gold</small>  
       </div>
-      <img src="https://i.pinimg.com/736x/58/e4/ed/58e4edebd8fd1f52b82809e522735d20.jpg" alt="logo"/>
+      <img src={logoPrincipal} alt="logo"/>
       <span className="mdi mdi-chevron-down text-white"></span>
       </div>
     </header>
