2aad938e15239f9ade2af2a9410098aa09c6ecef
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-23 15:04:48 -0300

    FIX: improvement in ReactDOM.render

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
