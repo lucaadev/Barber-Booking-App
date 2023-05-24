f6953990d69382dfab430ac49c9ccd8052929c9f
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-24 14:43:06 -0300

    FEAT: create agendamento redux-saga

--- web/src/store/modules/agendamento/sagas.js
+++ web/src/store/modules/agendamento/sagas.js
@@ -1,5 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call } from 'redux-saga/effects';
+import api from '../../../services/api';
+import consts from '../../../consts';
 
-export default all([
-  
-]);
+export function* filterAgendamento({ payload: { start, end } }) {
+  try {
+    const res = yield call(api.get, '/agendamento/filter', {
+      salaoId: consts.salaoId,
+      periodo: {
+        inicio: start,
+        fim: end,
+      }
+    });
+
+    console.log(res.data);
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export default function* agendamentoSaga() {
+  yield takeLatest('@agendamento/filterAgendamento', filterAgendamento);
+}
