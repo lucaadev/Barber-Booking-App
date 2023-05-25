dd83dd1399ab91aeeded7c7c0d6e40fb3aaac25d
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-25 19:02:06 -0300

    FEAT: add links & logo in sidebar component

--- web/src/components/Sidebar/index.jsx
+++ web/src/components/Sidebar/index.jsx
@@ -1,30 +1,30 @@
 import { Link, useLocation } from 'react-router-dom';
 
+import logoPrincipal from '../../assets/logo-principal.png';
+
 const Sidebar = () => {
 	const location = useLocation();
 
 	return (
 		<aside className='col-2 h-100'>
-				<h1 className=' texto px-2 py-4 text-center text-white'>Barbearia Lisboa</h1>
-			<div>
-				<ul className='p-0 m-0'>
-					<li>
-						<Link to='/' className={location.pathname === '/' ? 'active' : ''}>
-							<span className='mdi mdi-calendar-check'></span>
-							<span>Agendamentos</span>
-						</Link>
-					</li>
-					<li>
-						<Link
-							to='/clientes'
-							className={location.pathname === '/clientes' ? 'active' : ''}
-						>
-							<span className='mdi mdi-account-multiple'></span>
-							<span>Clientes</span>
-						</Link>
-					</li>
-				</ul>
-			</div>
+			<img src={logoPrincipal} alt='logo' className='img-fluid px-3 py-4'/>
+			<ul className='p-0 m-0'>
+				<li>
+					<Link to='/' className={location.pathname === '/' ? 'active' : ''}>
+						<span className='mdi mdi-calendar-check'></span>
+						<span>Agendamentos</span>
+					</Link>
+				</li>
+				<li>
+					<Link
+						to='/clientes'
+						className={location.pathname === '/clientes' ? 'active' : ''}
+					>
+						<span className='mdi mdi-account-multiple'></span>
+						<span>Clientes</span>
+					</Link>
+				</li>
+			</ul>
 		</aside>
 	);
 };
