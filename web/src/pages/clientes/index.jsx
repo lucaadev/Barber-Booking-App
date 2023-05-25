e26fb75b75b6681d039e5bacb8da71ffe6b78fb8
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-25 11:54:02 -0300

    FIX: css clientes page

--- web/src/pages/clientes/index.jsx
+++ web/src/pages/clientes/index.jsx
@@ -1,9 +1,9 @@
 const Clientes = () => {
 	return (
-		<div className='p-5 overflow-auto h-100'>
+		<div className='p-5 mt-5 overflow-auto h-100'>
 			<div className='row'>
 				<div className='col-12'>
-					<h2 className='mb-4 mt-0'>Clientes</h2>
+					<h2 className='mb-4 mt-0 text-center'>Clientes</h2>
 				</div>
 			</div>
 		</div>
