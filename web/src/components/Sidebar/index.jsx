0f3eb781008d5ef5ffe056406106289ad89d7b35
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-05-25 11:52:08 -0300

    FIX: css sidebar

--- web/src/components/Sidebar/index.jsx
+++ web/src/components/Sidebar/index.jsx
@@ -1,27 +1,32 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
 const Sidebar = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <aside className="col-2 h-100">
-      <h1 className="px-2 py-4 text-center text-white">Barbearia Lisboa</h1>
-      <ul className="p-0 m-0">
-        <li>
-          <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
-            <span className="mdi mdi-calendar-check"></span>
-            <span>Agendamentos</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/clientes" className={location.pathname === '/clientes' ? 'active' : ''}>
-            <span className="mdi mdi-account-multiple"></span>
-            <span>Clientes</span>
-          </Link>
-        </li>
-      </ul>
-    </aside>
-  );
+	return (
+		<aside className='col-2 h-100'>
+				<h1 className=' texto px-2 py-4 text-center text-white'>Barbearia Lisboa</h1>
+			<div>
+				<ul className='p-0 m-0'>
+					<li>
+						<Link to='/' className={location.pathname === '/' ? 'active' : ''}>
+							<span className='mdi mdi-calendar-check'></span>
+							<span>Agendamentos</span>
+						</Link>
+					</li>
+					<li>
+						<Link
+							to='/clientes'
+							className={location.pathname === '/clientes' ? 'active' : ''}
+						>
+							<span className='mdi mdi-account-multiple'></span>
+							<span>Clientes</span>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</aside>
+	);
 };
 
 export default Sidebar;
