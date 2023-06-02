a15e998f1d2884867d4338953391f49388bc6107
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-06-02 15:31:28 -0300

    FEAT: servico reducer

---
+++ web/src/store/modules/servico/reducer.js
@@ -0,0 +1,40 @@
+import { createReducer } from '@reduxjs/toolkit';
+import types from './types';
+import moment from 'moment';
+
+const initialState = {
+  behavior: 'create',
+  components: {
+    drawer: false,
+    confirmDelete: false,
+  },
+  form: {
+    filtering: false,
+    saving: false,
+    disabled: true,
+  },
+  servicos: [],
+  servico: {
+    nome: '',
+    valor: '',
+    duracao: moment('00:20', 'HH:mm').format(),
+    recorrencia: '',
+    descricao: '',
+    status: 'A',
+    arquivos: [],
+  }
+};
+
+export const servicoReducer = createReducer(initialState, (builder) => {
+  builder.addCase(types.UPDATE_SERVICO, (state, action) => {
+    return {
+      ...state,
+      ...action.payload,
+    };
+  }).addCase(types.RESET_SERVICO, (state) => {
+    return {
+      ...state,
+      ...initialState.servico,
+    };
+  }).addDefaultCase((state) => state);
+});
