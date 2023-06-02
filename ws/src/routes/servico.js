c9106ba888478debd040494e8311049d43714d4f
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-06-02 14:43:08 -0300

    FIX: method http for removeArquivo route

--- ws/src/routes/servico.js
+++ ws/src/routes/servico.js
@@ -4,9 +4,12 @@ const servicoController = require('../controller/servico');
 const router = express.Router();
 
 router.get('/salao/:salaoId', servicoController.getServicosBySalaoId);
+
 router.post('/', servicoController.newServico);
+router.post('/delete-arquivo', servicoController.removeArquivo);
+
 router.put('/:id', servicoController.updatedServico);
-router.delete('/delete-arquivo', servicoController.removeArquivo);
+
 router.delete('/:id', servicoController.removeServico);
 
 
