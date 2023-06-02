dd73657213e96c632af587bec7065fabfb32c852
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-06-02 15:32:34 -0300

    FEAT: add servico reducer & saga in yours roots

--- web/src/store/rootReducer.js
+++ web/src/store/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { agendamentoReducer } from "./modules/agendamento/reducer";
 import { clienteReducer } from "./modules/cliente/reducer";
 import { colaboradorReducer } from "./modules/colaborador/reducer";
+import { servicoReducer } from "./modules/servico/reducer";
 
 export default combineReducers({
   agendamentoReducer,
   clienteReducer,
   colaboradorReducer,
+  servicoReducer,
 });
