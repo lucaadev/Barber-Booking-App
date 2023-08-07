302b12e5491ecfb2d1ddb05bf8e510a8d250713b
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-08-07 16:27:58 -0300

    infos

---
+++ app/src/consts.js
@@ -0,0 +1,7 @@
+const infos = {
+  salaoId: '64529ff05928d28005a60ebd',
+  clienteId: '64663964c88fcaf1c318da9d',
+  logo: 'https://barbearialisboa.ageenda.com.br/wp-content/uploads/Logo.png',
+};
+
+export default infos;
