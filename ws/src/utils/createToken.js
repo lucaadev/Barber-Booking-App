a1dfd8e1a7ac1dbbdd697376ad134b16eaa8fcd7
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-08-20 20:12:23 -0300

    FIX: update createToken function

--- ws/src/utils/createToken.js
+++ ws/src/utils/createToken.js
@@ -1,7 +1,18 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { readFileSync } = require('fs');
+const path = require('path');
 
-const secretKey = readFileSync('./jwt.evaluation.key', 'utf8');
+let secretKey;
+
+try {
+    // Tenta ler a chave secreta do arquivo jwt.evaluation.key
+    const keyPath = path.join(__dirname, 'jwt.evaluation.key');
+    secretKey = readFileSync(keyPath, 'utf8');
+} catch (error) {
+    // Se houver um erro ao ler o arquivo, tenta usar a variável de ambiente SECRET_KEY
+    secretKey = process.env.SECRET_KEY;
+}
 
 const configJWT = {
     expiresIn: '30d',
@@ -10,7 +21,7 @@ const configJWT = {
 
 const generateToken = (payload) => jwt.sign({ payload }, secretKey, configJWT);
 
-const createToken = (data) => { 
+const createToken = (data) => {
     const token = generateToken(data);
     return { token, ...data };
 };
