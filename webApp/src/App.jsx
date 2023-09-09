d9e90dcdda17c06da44eece8db40cd4d452eaa48
Lucas Junqueira Cavalcante Lima <lucascavalcantelima21@gmail.com>
2023-09-08 22:19:39 -0300

    FEAT: cadastro page

--- webApp/src/App.jsx
+++ webApp/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 
-import Login from './pages/Login'
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" exact element={<Login/>} />
-        <Route path="/Cadastro" element={<h1>Cadastro</h1>} />
+        <Route path="/Cadastro" element={<Cadastro/>} />
         <Route path="/Home" element={<h1>Home</h1>} />
       </Routes>
     </>
